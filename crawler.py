be8ca8c18a1cc1fc17204053493d86edf76f5f05
meatball0520 <meatball0520@gmail.com>
2021-02-03 22:51:39 +0800

    chore: add cralwer

---
+++ crawler.py
@@ -0,0 +1,34 @@
+import requests
+import re
+
+
+class SubTitleCralwer:
+
+    def __init__(self, v: str):
+        self.en_url = None
+        self.zh_url = None
+        self.url = f'https://www.youtube.com/watch?v={v}'
+        self.en_subtitles = []
+        self.zh_subtitles = []
+
+    def init(self):
+        self.get_api_urls()
+
+    def get_api_urls(self):
+        url = self.url
+        r = requests.get(url)
+        content = r.text
+        api = re.findall(r'"(https://www.youtube.com/api/timedtext.+?)"', content)[-1]
+        self.en_url = re.sub(r'\\u0026', '&', api)
+        self.zh_url = self.en_url + '&tlang=zh-Hant'
+
+    def get_list(self, content):
+        content = re.sub(r'&amp;#39;', '\'', content)
+        return re.findall(r'<.+?">(.+?)</text', content)
+
+    def get_subtitle(self):
+        r = requests.get(self.en_url)
+        self.en_subtitles = self.get_list(r.text)
+
+        r = requests.get(self.zh_url)
+        self.zh_subtitles = self.get_list(r.text)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import jp
 import uvicorn
 import re
 import random
+from crawler import SubTitleCralwer
 
 
 class Word(jp.Span):
@@ -10,16 +11,49 @@ class Word(jp.Span):
         super().__init__(**kwargs)
 
 
-class WordInput(jp.Input):
+class WordInput(jp.InputChangeOnly):
     def __init__(self, length=1, **kwargs):
         kwargs['class_'] = 'outline-none bg-gray-300 text-green-500 text-5xl'
         kwargs['style'] = f'width: {length * 2}rem;'
         super().__init__(**kwargs)
 
 
+class WatchCard(jp.Div):
+    def __init__(self, **kwargs):
+        self.wp = None
+        self.citem = None
+        kwargs['class_'] = 'w-2/3 bg-white mt-20  rounded-lg shadow p-12'
+        kwargs['style'] = 'min-height: 20rem;'
+        super().__init__(**kwargs)
+        input_ = WordInput(length=10)
+        button = jp.Button(
+            class_='text-5xl px-6 m-2 text-lg text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-80',
+            text='送出', click=self.click)
+        self.add_component(input_)
+        self.add_component(button)
+        self.input = input_
+
+    def click(self, _):
+        if self.input.value:
+            self.wp.watch = self.input.value
+            cralwer = SubTitleCralwer(self.input.value)
+            cralwer.init()
+            cralwer.get_subtitle()
+            self.citem.delete()
+            card = Card(wp=self.wp, crawler=cralwer)
+            card.build()
+            self.citem.add_component(card)
+
+
 class Card(jp.Div):
-    def __init__(self, sentence, **kwargs):
-        self.sentence = sentence
+    def __init__(self, wp, crawler: SubTitleCralwer, **kwargs):
+        self.wp = wp
+        self.answer = None
+        self.en = None
+        self.tw = None
+        self.crawler = crawler
+        self.total_count = len(self.crawler.en_subtitles)
+        self.count_index = 0
         kwargs['class_'] = 'w-2/3 bg-white mt-20  rounded-lg shadow p-12'
         kwargs['style'] = 'min-height: 20rem;'
         super().__init__(**kwargs)
@@ -32,35 +66,72 @@ class Card(jp.Div):
             if len(word) >= 2 or count > 3:
                 return word
 
-    def react(self):
+    async def change(self, msg):
+        if msg.value == self.answer:
+            await self.build()
+        else:
+            msg.target.value = ''
+        print('enter!!')
+
+    async def make_sound(self):
+        eval_text = f"""
+            let utterance = new window.SpeechSynthesisUtterance('{self.en}');
+            utterance.lang = 'en-US';
+            window.speechSynthesis.speak(utterance)
+            """
+        await self.wp.run_javascript(eval_text)
+        print('after run')
+        print(eval_text)
+
+    async def build(self):
         self.delete_components()
-        s = self.sentence
-        words = re.findall(r'\w+', s)
+        en = self.crawler.en_subtitles[self.count_index]
+        tw = self.crawler.zh_subtitles[self.count_index]
+        self.en = en
+        self.tw = tw
+        words = re.findall(r'\w+', en)
         word = self.get_word(words)
-        st_index = s.index(word)
+        self.answer = word
+        st_index = en.index(word)
         ed_index = st_index + len(word)
-        prefix_s = s[:st_index]
-        suffix_s = s[ed_index:]
+        prefix_s = en[:st_index]
+        suffix_s = en[ed_index:]
         self.add_component(Word(text=prefix_s))
-        self.add_component(WordInput(length=len(word)))
+        self.add_component(
+            WordInput(length=len(word), change=self.change)
+        )
         self.add_component(Word(text=suffix_s))
+        self.add_component(jp.Div(class_='bg-gray-600 h-px my-6'))
+        self.add_component(jp.Div(class_='text-blue-700', text=self.tw))
+
+        self.count_index += 1
         print('prefix_s:', prefix_s)
         print('word:', word)
         print('suffix_s:', suffix_s)
+        await self.make_sound()
 
 
 @jp.SetRoute('/')
-def demo():
+async def demo():
     wp = jp.WebPage()
+    await wp.run_javascript("""
+    let utterance = new window.SpeechSynthesisUtterance('[Music]');
+    utterance.lang = 'en-US';
+    window.speechSynthesis.speak(utterance);
+    console.log('yooo');
+    
+    """)
     c = jp.parse_html("""
     <div class="bg-red-200 h-screen">
         <div class="flex flex-col items-center" name="item">
         </div>
       </div>
     """)
-    citem = c.name_dict['item']
-    card = Card(sentence='Tom works like a horse.')
-    citem.add_component(card)
+    # citem = c.name_dict['item']
+    # watchcard = WatchCard(wp=wp, citem=citem)
+    # citem.add_component(watchcard)
+    # # card = Card(sentence='Tom works like a horse.')
+    # # citem.add_component(card)
     wp.add_component(c)
 
     return wp
